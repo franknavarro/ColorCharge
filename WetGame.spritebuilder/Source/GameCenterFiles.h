bb2830d401f0a24edf6123fea027e9838c585ebc
Frank Navarro-Velasco <Frank@Franks-MacBook-Pro.local>
2014-07-30 18:14:45 -0700

    Added GameCenter and share

---
+++ WetGame.spritebuilder/Source/GameCenterFiles.h
@@ -0,0 +1,13 @@
+//
+//  GameCenterFiles.h
+//  Color Charge
+//
+//  Created by Frank Navarro-Velasco on 7/30/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameCenterFiles : NSObject
+
+@end
---
+++ WetGame.spritebuilder/Source/GameCenterFiles.m
@@ -0,0 +1,13 @@
+//
+//  GameCenterFiles.m
+//  Color Charge
+//
+//  Created by Frank Navarro-Velasco on 7/30/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "GameCenterFiles.h"
+
+@implementation GameCenterFiles
+
+@end
